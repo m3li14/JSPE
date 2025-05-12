2539cc741c5af75dfed81426857b5128b6673950
m3li14 <m3li14@gmail.com>
2025-05-12 11:31:44 -0400

    bookSystem

--- seatBooking/booking.js
+++ seatBooking/booking.js
@@ -42,3 +42,35 @@ function bookRandomSeat() {
 		alert('All seats are already booked.')
 	}
 }
+
+class person {
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	getFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+const Person1 = new person("Aly", "Hussin");
+console.log(Person1.getFullName());
+
+const myData = {
+	firstName: "Mohamed",
+	lastName: "Hussin",
+	yearBorn: "1979",
+	age: 46
+};
+console.log(`${myData.firstName}, ${myData.lastName} was born in ${myData.yearBorn} so he's ${myData.age}`);
+
+const students = [
+	{ name: "Alic", age: 25 },
+	{ name: "Bob", age: 27 },
+	{ name: "Goat", age: 29 }
+];
+
+for (let i = 0; i < students.length; i++) {
+	console.log(`${students[i].name} is ${students[i].age} years old`);
+}
+
